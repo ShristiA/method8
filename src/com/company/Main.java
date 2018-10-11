e85e8488584bb2610e9e7ab8dad93bf049f9391f
ShristiA <shristiamatya3@gmail.com>
2018-10-11 10:37:17 -0400

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-	int a = randomMethod();
-        System.out.println(a);
+//	int a = randomMethod();
+//        System.out.println(a);
+//        System.out.println(randomMethod());
+        randomMethod();
     }
     public static int randomMethod(){
         int x = 0 ;
         Random r = new Random();
         for(int i = 0; i<5; i++) {
             x = 1 + r.nextInt(5);
+            System.out.println(x);
         }
         return x;
 
